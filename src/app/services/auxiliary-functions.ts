857d5bbb76d16bed735bc10d59ea0ddf96a3adc2
João Augusto Rezende <joaoaugustorezende@MacBook-Air-de-Joao-3.local>
2025-11-27 22:18:16 -0300

    funcoes de bloco de processamento pontual finalizadas

--- src/app/services/auxiliary-functions.ts
+++ src/app/services/auxiliary-functions.ts
@@ -4,5 +4,30 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuxiliaryFunctions {
-  
+  adjustBrightness(imageData: Uint8Array, brightness: number, isSubtract: boolean): Uint8Array {
+    const output = new Uint8Array(imageData.length);
+
+    for (let i = 0; i < imageData.length; i++) {
+      let newValue = isSubtract ? imageData[i] - (imageData[i] / 100 * brightness) : imageData[i] + (imageData[i] / 100 * brightness);
+
+      if (newValue > 255) newValue = 255;
+      else if (newValue < 0) newValue = 0;
+
+      output[i] = newValue;
+    }
+
+    return output;
+  }
+
+  threshold(imageData: Uint8Array, threshold: number): Uint8Array {
+    const output = new Uint8Array(imageData.length);
+
+    for (let i = 0; i < imageData.length; i++) {
+      const pixel = imageData[i];
+      output[i] = pixel >= threshold ? 255 : 0;
+    }
+
+    return output;
+  }
+
 }
